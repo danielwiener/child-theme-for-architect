b0ea20395a7d28b49b0953fc04685933054a2e13
danielwiener <daniel@danielwiener.com>
2011-01-21 17:23:22 -0500

    disabled the update notice except for me

--- functions.php
+++ functions.php
@@ -32,6 +32,12 @@ function enable_threaded_comments(){
 }
 add_action('get_header', 'enable_threaded_comments');
 
+// kill the admin nag
+if (!current_user_can('edit_users')) {
+	add_action('init', create_function('$a', "remove_action('init', 'wp_version_check');"), 2);
+	add_filter('pre_option_update_core', create_function('$a', "return null;"));
+}
+
 
 if ( ! function_exists( 'twentyten_posted_on' ) ) :
 /**
